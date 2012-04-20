6dbe08850d3c29c60dcc560b05c7cf6337317233
Wes <hi@wes-waugh.com>
2012-04-20 10:48:43 -0700

    Initial Commit

---
+++ ios-gl-4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-gl-4
+//
+//  Created by Wesley Waugh on 20/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
