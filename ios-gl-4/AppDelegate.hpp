1018b70de9a6b3d90ddc798ab435966893d781bf
Wes <hi@wes-waugh.com>
2012-04-21 12:44:23 -0700

    Builds, but doesn't display anything

--- ios-gl-4/AppDelegate.hpp
+++ ios-gl-4/AppDelegate.hpp
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "system/GLView.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate> {
+    GLView *m_view;
+}
 
-@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIWindow *m_window;
 
 @end
--- ios-gl-4/AppDelegate.mm
+++ ios-gl-4/AppDelegate.mm
@@ -6,57 +6,73 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "AppDelegate.hpp"
+#import "game/game.hpp"
 
 @implementation AppDelegate
 
-@synthesize window = _window;
+@synthesize m_window;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+
+/******************************************************************************
+ * Main Entry point. See `system/GLView.mm` for the main loop.
+ *****************************************************************************/
+- (BOOL)application:(UIApplication *)application
+  didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    CGRect screen = [[UIScreen mainScreen] bounds];
+    
+    m_window = [[UIWindow alloc] initWithFrame:screen];
+    m_view = [[GLView alloc] initWithFrame:screen];
+    
+    [m_window addSubview:m_view];
+    [m_window makeKeyAndVisible];
+
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (void)applicationWillResignActive:(UIApplication *)application {
     /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     Sent when the application is about to move from active to inactive state.
+     This can occur for certain types of temporary interruptions (such as an
+     incoming phone call or SMS message) or when the user quits the application
+     and it begins the transition to the background state.  Use this method to
+     pause ongoing tasks, disable timers, and throttle down OpenGL ES frame
+     rates. Games should use this method to pause the game.
      */
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+     Use this method to release shared resources, save user data, invalidate
+     timers, and store enough application state information to restore your
+     application to its current state in case it is terminated later.  If your
+     application supports background execution, this method is called instead
+     of applicationWillTerminate: when the user quits.
      */
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+     Called as part of the transition from the background to the inactive
+     state; here you can undo many of the changes made on entering the
+     background.
      */
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     Restart any tasks that were paused (or not yet started) while the
+     application was inactive. If the application was previously in the
+     background, optionally refresh the user interface.
      */
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
+     Called when the application is about to terminate.  Save data if
+     appropriate.  See also applicationDidEnterBackground:.
      */
 }
 
